d0f91b8f1e2a3e6981f3b628a551fdbec99bdef2
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-10-27 19:58:41 -0300

    Initialize topology on main

--- router.py
+++ router.py
@@ -1,8 +1,10 @@
 import sys
+from topology import Topology
 
 def main():
     print('Number of arguments:', len(sys.argv), 'arguments.')
     print('Argument List:', str(sys.argv))
+    topology = Topology()
 
 if __name__ == "__main__":
     main()
