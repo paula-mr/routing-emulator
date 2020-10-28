ec044cdcb3a18cfc54f36ba599e7785905567389
Joao Pedro Renan <jota.renan@gmail.com>
2020-10-27 23:28:52 -0300

    criaçao de mensagem de update

--- router.py
+++ router.py
@@ -1,11 +1,17 @@
 import sys
 from neighbors import Neighbors
 from routing_table import RoutingTable
+from message import UpdateMessage
 
 
 def main():
     print("Number of arguments:", len(sys.argv), "arguments.")
     print("Argument List:", str(sys.argv))
+    address = sys.argv[1]
+    pi_period = sys.argv[2]
+    if len(sys.argv) == 4:
+        filepath = sys.argv[3]
+        # todo: abrir e ler arquivo
     neighbors = Neighbors()
     routing_table = RoutingTable()
     while True:
@@ -27,6 +33,12 @@ def main():
         else:
             print("comando errado")
             return
+
+
+def create_update_message(table, source, destination_ip):
+    message = UpdateMessage(source, destination_ip)
+    message.distances = table.generate_distances(destination_ip)
+    return message
 
 
 if __name__ == "__main__":
