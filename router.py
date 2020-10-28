d88064375217f3f7f7f376235cb6ca40d2b1aa62
Joao Pedro Renan <jota.renan@gmail.com>
2020-10-27 22:06:46 -0300

    comando quit

--- router.py
+++ router.py
@@ -2,12 +2,17 @@ import sys
 from neighbors import Neighbors
 from routing_table import RoutingTable
 
+
 def main():
-    print('Number of arguments:', len(sys.argv), 'arguments.')
-    print('Argument List:', str(sys.argv))
+    print("Number of arguments:", len(sys.argv), "arguments.")
+    print("Argument List:", str(sys.argv))
     neighbors = Neighbors()
     routing_table = RoutingTable()
+    while True:
+        command = input()
+        if command == "quit":
+            return
+
 
 if __name__ == "__main__":
     main()
-    
