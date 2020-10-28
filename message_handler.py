3ea45534a067d37339dab029f88ea1bf2d9cd36e
Joao Pedro Renan <jota.renan@gmail.com>
2020-10-28 19:05:40 -0300

    message handling

--- message_handler.py
+++ message_handler.py
@@ -1,10 +1,25 @@
+from message import DataMessage
+import json
+
+
 class MessageHandler:
     def __init__(self):
         pass
 
     @staticmethod
-    def handle_trace(message):
-        pass
+    def handle_trace(message, current_ip):
+        # adicionar proprio ip à lista de hops do trace (message.hops)
+        message.hops.append(current_ip)
+        # verificar se é destino do trace
+        if message.destination != current_ip:
+            # se não for destino: enviar pelo caminho mais curto ao destino
+            pass
+        else:
+            # se for destino: enviar mensagem de data para a origem. Payload = json da propria msg de trace
+            data_message = DataMessage(current_ip, message.source)
+            data_message.payload = json.dumps(message.__dict__)
+            # todo: enviar a mensagem
+            pass
 
     @staticmethod
     def handle_update(message):
@@ -12,7 +27,7 @@ class MessageHandler:
 
     @staticmethod
     def handle_data(message):
-        pass
+        print(message.payload)
 
     @staticmethod
     def message_handler_dic(message_type):
