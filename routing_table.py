ffc28e43971ca48f22b034635114e432acc2dced
Joao Pedro Renan <jota.renan@gmail.com>
2020-10-27 22:54:54 -0300

    info

--- routing_table.py
+++ routing_table.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from functools import reduce
 
 
 class RoutingInformation:
@@ -9,9 +10,25 @@ class RoutingInformation:
         self.next_hop = next_hop
 
 
+def passes_split_horizon(destination_ip):
+    def for_ip(k, v):
+        return k != destination_ip and v.source_ip != destination_ip
+
+    return for_ip
+
+
 class RoutingTable:
     def __init__(self):
         self.links = {}
-    
+
     def add(self, ip, weight, source_ip, next_hop):
         self.links[ip] = RoutingInformation(weight, source_ip, next_hop)
+
+    def split_horizon(self, table, destination_ip):
+        return filter(passes_split_horizon(destination_ip), table.items())
+
+    def generate_distances(self, table, destination_ip):
+        def extract_info(dic, table_row_info):
+            dic[table_row_info.ip] = table_row_info.weight + self.links[destination_ip]
+
+        return reduce(extract_info, self.split_horizon(table, destination_ip), {})
