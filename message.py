fc94515379ace0f843f284d5988b29dcd1b455a5
Paula Ribeiro <paula.mribeiro05@gmail.com>
2020-10-28 22:15:00 -0300

    Adds serialize method to Message class

--- message.py
+++ message.py
@@ -1,8 +1,13 @@
+import json
+
 class Message:
     def __init__(self, m_type, source, destination):
         self.type = m_type
         self.source = source
         self.destination = destination
+    
+    def serialize(self):
+        return json.dumps(self.__dict__)
 
 
 class DataMessage(Message):
